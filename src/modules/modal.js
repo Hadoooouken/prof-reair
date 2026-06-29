c1b5d3e652966e48a088c8723138a51cf19171dc
Filipp <wolverinexdd@yandex.ru>
2026-06-29 22:41:59 +0400

    переработана логика открытия и закрытия  модальных окон

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,10 +1,12 @@
 export const modal = () => {
   const headerModal = document.querySelector('.header-modal');
-  const closeButton = document.querySelector('.header-modal__close');
+  const serviceCardContainer = document.querySelector('.service-slider__track');
+  const closeButtonAll = document.querySelectorAll('.close-modal');
   const modalButton = document.querySelector('.button');
   const overlay = document.querySelector('.overlay');
   const documentsSection = document.querySelector('#documents');
-
+  const servicesModal = document.querySelector('.services-modal');
+  console.log(closeButtonAll);
   const imageModal = document.querySelector('.image-popup');
   const imageModalImage = imageModal.querySelector('.popup__image');
 
@@ -34,21 +36,32 @@ export const modal = () => {
     }
   };
 
-  modalButton.addEventListener('click', (e) => {
-    openModal(headerModal);
-  });
-
-  closeButton.addEventListener('click', () => {
-    closeModal(headerModal);
-  });
-
-  overlay.addEventListener('click', closeModalByOverlay);
-
   function openImageModal(card) {
     imageModalImage.src = card.href;
 
     openModal(imageModal);
   }
+
+  modalButton.addEventListener('click', (e) => {
+    openModal(headerModal);
+  });
+
+  closeButtonAll.forEach((closeButton) => {
+    closeButton.addEventListener('click', (e) => {
+      const modal = e.target.closest('.modal-opened');
+      if (modal) {
+        closeModal(modal);
+      }
+    });
+  });
+
+  overlay.addEventListener('click', closeModalByOverlay);
+
+  serviceCardContainer.addEventListener('click', (e) => {
+    if (e.target.closest('.service-button')) {
+      openModal(servicesModal);
+    }
+  });
 
   documentsSection.addEventListener('click', (e) => {
     e.preventDefault();
--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,61 +1,3 @@
-// export const slider = () => {
-//   const benefitsWrap = document.querySelector('.benefits-wrap');
-//   const benefitsArrows = document.querySelector('.benefits-arrows');
-//   const buttonLeft = document.querySelector('.benefits__arrow--left');
-//   const buttonRight = document.querySelector('.benefits__arrow--right');
-//   const slideItem = document.querySelector('.benefits__item');
-//   const benefitInner = document.querySelector('.benefits-inner');
-
-//   let benefitInnerWidth = benefitInner.clientWidth;
-//   let step = 3;
-//   let offset = 0;
-//   let slideItemWidth = slideItem.offsetWidth;
-
-//   // const res = benefitInnerWidth / slideItemWidth
-
-//   const disableButtons = () => {
-//     if (slideItemWidth < 180) {
-//       step = 5;
-//     } else {
-//       step = 3;
-//     }
-
-//     if (offset <= -slideItemWidth * step) {
-//       buttonRight.classList.add('benefits__arrow--disabled');
-//     } else {
-//       buttonRight.classList.remove('benefits__arrow--disabled');
-//     }
-//     if (offset >= 0) {
-//       buttonLeft.classList.add('benefits__arrow--disabled');
-//     } else {
-//       buttonLeft.classList.remove('benefits__arrow--disabled');
-//     }
-//   };
-
-//   disableButtons();
-
-//   window.addEventListener('resize', (e) => {
-//     slideItemWidth = slideItem.offsetWidth;
-//     offset = 0;
-//     benefitsWrap.style.transform = `translateX(${offset}px)`;
-//     disableButtons();
-//   });
-
-//   benefitsArrows.addEventListener('click', (e) => {
-//     if (e.target.closest('.benefits__arrow--right')) {
-//       offset += -slideItemWidth;
-//       benefitsWrap.style.transform = `translateX(${offset}px)`;
-//       console.log(offset);
-//       disableButtons();
-//     } else if (e.target.closest('.benefits__arrow--left')) {
-//       offset += slideItemWidth;
-//       benefitsWrap.style.transform = `translateX(${offset}px)`;
-//       console.log(offset);
-//       disableButtons();
-//     }
-//   });
-// };
-
 export const slider = (config) => {
   const sliderTrack = document.querySelector(config.sliderTrack);
   const sliderArrows = document.querySelector(config.sliderArrows);
@@ -65,28 +7,8 @@ export const slider = (config) => {
   const slider = document.querySelector(config.slider);
   const allSliderItems = document.querySelectorAll(config.allSliderItems);
 
-console.log('sliderTrack', sliderTrack);
-console.log('sliderArrows', sliderArrows);
-console.log('buttonLeft', buttonLeft);
-console.log('buttonRight', buttonRight);
-console.log('slideItem', slideItem);
-console.log('slider', slider);
-console.log('allSliderItems', allSliderItems);
-
-// if (
-//   !sliderTrack ||
-//   !sliderArrows ||
-//   !buttonLeft ||
-//   !buttonRight ||
-//   !slideItem ||
-//   !slider ||
-//   !allSliderItems.length
-// ) {
-//   return;
-// }
   let offset = 0;
   let slideItemWidth = slideItem.offsetWidth;
-  console.log(slider.clientWidth / slideItemWidth);
 
   const getMaxStep = () => {
     slideItemWidth = slideItem.offsetWidth;
