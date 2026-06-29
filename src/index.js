1a352957bc8c6ff3d286bdd7ad7b939e8ebb5848
Filipp <wolverinexdd@yandex.ru>
2026-06-29 22:05:35 +0400

    улучшена логика слайдера, теперь слайдер универсальный

--- src/index.js
+++ src/index.js
@@ -6,13 +6,27 @@ import '../css/style.css';
 import { modal } from './modules/modal';
 import { slider } from './modules/slider';
 import { timer } from './modules/timer';
-import { smothScroll } from './smothScroll';
+import { smothScroll } from './modules/smothScroll';
 
-modal()
-slider()
-timer('28 jule 2026')
-smothScroll()
+modal();
+slider({
+  sliderTrack: '.service-slider__track',
+  sliderArrows: '.services-arrows',
+  buttonLeft: '.services__arrow--left',
+  buttonRight: '.services__arrow--right',
+  slideItem: '.services__item',
+  slider: '.service-slider',
+  allSliderItems: '.services__item',
+});
+slider({
+  sliderTrack: '.benefits-wrap',
+  sliderArrows: '.benefits-arrows',
+  buttonLeft: '.benefits__arrow--left',
+  buttonRight: '.benefits__arrow--right',
+  slideItem: '.benefits__item',
+  slider: '.benefits-inner',
+  allSliderItems: '.benefits__item',
+});
 
-// document.querySelector('img').addEventListener('click', function() {
-//     this.requestFullscreen();
-// });
+timer('28 jule 2026');
+smothScroll();
--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,34 +1,112 @@
-export const slider = () => {
-  const benefitsWrap = document.querySelector('.benefits-wrap');
-  const benefitsArrows = document.querySelector('.benefits-arrows');
-  const buttonLeft = document.querySelector('.benefits__arrow--left');
-  const buttonRight = document.querySelector('.benefits__arrow--right');
-  const slideItem = document.querySelector('.benefits__item');
-  const benefitInner = document.querySelector('.benefits-inner');
+// export const slider = () => {
+//   const benefitsWrap = document.querySelector('.benefits-wrap');
+//   const benefitsArrows = document.querySelector('.benefits-arrows');
+//   const buttonLeft = document.querySelector('.benefits__arrow--left');
+//   const buttonRight = document.querySelector('.benefits__arrow--right');
+//   const slideItem = document.querySelector('.benefits__item');
+//   const benefitInner = document.querySelector('.benefits-inner');
 
-  let benefitInnerWidth = benefitInner.clientWidth;
-  let step = 3;
+//   let benefitInnerWidth = benefitInner.clientWidth;
+//   let step = 3;
+//   let offset = 0;
+//   let slideItemWidth = slideItem.offsetWidth;
+
+//   // const res = benefitInnerWidth / slideItemWidth
+
+//   const disableButtons = () => {
+//     if (slideItemWidth < 180) {
+//       step = 5;
+//     } else {
+//       step = 3;
+//     }
+
+//     if (offset <= -slideItemWidth * step) {
+//       buttonRight.classList.add('benefits__arrow--disabled');
+//     } else {
+//       buttonRight.classList.remove('benefits__arrow--disabled');
+//     }
+//     if (offset >= 0) {
+//       buttonLeft.classList.add('benefits__arrow--disabled');
+//     } else {
+//       buttonLeft.classList.remove('benefits__arrow--disabled');
+//     }
+//   };
+
+//   disableButtons();
+
+//   window.addEventListener('resize', (e) => {
+//     slideItemWidth = slideItem.offsetWidth;
+//     offset = 0;
+//     benefitsWrap.style.transform = `translateX(${offset}px)`;
+//     disableButtons();
+//   });
+
+//   benefitsArrows.addEventListener('click', (e) => {
+//     if (e.target.closest('.benefits__arrow--right')) {
+//       offset += -slideItemWidth;
+//       benefitsWrap.style.transform = `translateX(${offset}px)`;
+//       console.log(offset);
+//       disableButtons();
+//     } else if (e.target.closest('.benefits__arrow--left')) {
+//       offset += slideItemWidth;
+//       benefitsWrap.style.transform = `translateX(${offset}px)`;
+//       console.log(offset);
+//       disableButtons();
+//     }
+//   });
+// };
+
+export const slider = (config) => {
+  const sliderTrack = document.querySelector(config.sliderTrack);
+  const sliderArrows = document.querySelector(config.sliderArrows);
+  const buttonLeft = document.querySelector(config.buttonLeft);
+  const buttonRight = document.querySelector(config.buttonRight);
+  const slideItem = document.querySelector(config.slideItem);
+  const slider = document.querySelector(config.slider);
+  const allSliderItems = document.querySelectorAll(config.allSliderItems);
+
+console.log('sliderTrack', sliderTrack);
+console.log('sliderArrows', sliderArrows);
+console.log('buttonLeft', buttonLeft);
+console.log('buttonRight', buttonRight);
+console.log('slideItem', slideItem);
+console.log('slider', slider);
+console.log('allSliderItems', allSliderItems);
+
+// if (
+//   !sliderTrack ||
+//   !sliderArrows ||
+//   !buttonLeft ||
+//   !buttonRight ||
+//   !slideItem ||
+//   !slider ||
+//   !allSliderItems.length
+// ) {
+//   return;
+// }
   let offset = 0;
   let slideItemWidth = slideItem.offsetWidth;
+  console.log(slider.clientWidth / slideItemWidth);
 
-  // const res = benefitInnerWidth / slideItemWidth
+  const getMaxStep = () => {
+    slideItemWidth = slideItem.offsetWidth;
+
+    const visibleCards = Math.floor(slider.clientWidth / slideItemWidth);
+
+    return allSliderItems.length - visibleCards;
+  };
 
   const disableButtons = () => {
-    if (slideItemWidth < 180) {
-      step = 5;
+    const maxStep = getMaxStep();
+    if (offset <= -slideItemWidth * maxStep) {
+      buttonRight.classList.add('button__arrow--disabled');
     } else {
-      step = 3;
-    }
-
-    if (offset <= -slideItemWidth * step) {
-      buttonRight.classList.add('benefits__arrow--disabled');
-    } else {
-      buttonRight.classList.remove('benefits__arrow--disabled');
+      buttonRight.classList.remove('button__arrow--disabled');
     }
     if (offset >= 0) {
-      buttonLeft.classList.add('benefits__arrow--disabled');
+      buttonLeft.classList.add('button__arrow--disabled');
     } else {
-      buttonLeft.classList.remove('benefits__arrow--disabled');
+      buttonLeft.classList.remove('button__arrow--disabled');
     }
   };
 
@@ -37,20 +115,18 @@ export const slider = () => {
   window.addEventListener('resize', (e) => {
     slideItemWidth = slideItem.offsetWidth;
     offset = 0;
-    benefitsWrap.style.transform = `translateX(${offset}px)`;
+    sliderTrack.style.transform = `translateX(${offset}px)`;
     disableButtons();
   });
 
-  benefitsArrows.addEventListener('click', (e) => {
-    if (e.target.closest('.benefits__arrow--right')) {
+  sliderArrows.addEventListener('click', (e) => {
+    if (e.target.closest(config.buttonRight)) {
       offset += -slideItemWidth;
-      benefitsWrap.style.transform = `translateX(${offset}px)`;
-      console.log(offset);
+      sliderTrack.style.transform = `translateX(${offset}px)`;
       disableButtons();
-    } else if (e.target.closest('.benefits__arrow--left')) {
+    } else if (e.target.closest(config.buttonLeft)) {
       offset += slideItemWidth;
-      benefitsWrap.style.transform = `translateX(${offset}px)`;
-      console.log(offset);
+      sliderTrack.style.transform = `translateX(${offset}px)`;
       disableButtons();
     }
   });
